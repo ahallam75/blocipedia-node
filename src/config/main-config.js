44b4613621e4b98f6c65c51b27267271333f480c
ahallam <alexhallam75@gmail.com>
2018-10-31 15:18:24 -0400

    Working on nodemon

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const bodyParser = require("body-parser");
+const logger = require('morgan');
 
 module.exports = {
   init(){
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(logger('dev'));
 
   }
 };
