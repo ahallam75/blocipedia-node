87a400a841b98fa2e594a37eb79b5f7df1644204
ahallam <alexhallam75@gmail.com>
2018-11-05 09:39:30 -0500

    First commit: working on spec failure

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,3 @@
-module.exports = {
-  signUp(req, res, next){
-    res.render("users/sign_up");
-  }
-}
-
 const userQueries = require("../db/queries.users");
 const passport = require("passport");
 const sgMail = require('@sendgrid/mail');
@@ -41,5 +35,25 @@ module.exports = {
         })
       }
     });
+  }, 
+  signInForm(req, res, next){
+    res.render("users/signin");
+  },
+  signIn(req, res, next){
+    passport.authenticate("local")(req, res, function() {
+      if(!req.user){
+        req.flash("notice", "Sign in failed. Please try again.");
+        res.redirect("/users/signin");
+      }
+      else{
+        req.flash("notice", "You've successfully signed in!");
+        res.redirect("/");
+      }
+    })
+  },
+  signOut(req, res, next){
+    req.logout();
+    req.flash("notice", "You've successfully signed out");
+    res.redirect("/");
   }
 }
