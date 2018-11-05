118edb7dacc26df9bc1cd06da0da8caffb99e07d
ahallam <alexhallam75@gmail.com>
2018-11-05 15:58:28 -0500

    First commit: tests pass/working on TypeError

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,9 +1,11 @@
 module.exports = {
-    init(app){
-      const staticRoutes = require("../routes/static");
-      const userRoutes = require("../routes/users");
+  init(app){
+     const staticRoutes = require("../routes/static");
+     const userRoutes = require("../routes/users");
+     const wikiRoutes = require("../routes/wikis");
 
-      app.use(staticRoutes);
-      app.use(userRoutes);
-    }
+     app.use(staticRoutes);
+     app.use(userRoutes);
+     app.use(wikiRoutes);
   }
+}
